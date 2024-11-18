4dbbbc1ff767c851af863f7d1fd365e57d75957e
syevafabiano07 <syeva.dwi49@smk.belajar.id>
2024-11-18 08:19:35 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+class java_length{
+  public static void main (String[] args){
+    String nama1 = "Syeva dwi";
+    String nama2 = "Fabiano ";
+    
+    System.out.println("Panjang nama 1 : " + nama1.length());
+    System.out.println("Panjang nama 2 : " + nama2.length());
+  }
+}
