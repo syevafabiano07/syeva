cd6ef759b4e5cf1f7c74259a49446af6f4a209bd
syevafabiano07 <syeva.dwi49@smk.belajar.id>
2024-11-18 08:16:10 +0700

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,10 @@
+class Tipe_data_string {
+  public static void main(String[] args) {
+    String firtName = "Syeva";
+    String lastName = "Fabiano";
+    
+    System.out.println(firtName);
+    System.out.println(lastName);
+  }
+
+}
